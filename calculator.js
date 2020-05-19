efee13a51295975f7679b5a73dc4027fdead8976
murphy52589 <Kylemurphy52589@gmail.com>
2020-05-18 21:47:17 -0500

    done

--- calculator.js
+++ calculator.js
@@ -3,6 +3,17 @@
 function calculateTip() {
     const billAmount = document.getElementById('billAmount').value;
     const tipAmount = document.getElementById('tipAmount').value;
-    const numberOfPeople = document.getElementById('numberOfPeople').value;
-    console.log(billAmount, tipAmount, numberOfPeople);
+    let numberOfPeople = document.getElementById('numberOfPeople').value;
+
+    //validate input
+    if (billAmount === "" || tipAmount === 0) {
+        alert("Please enter values");
+        return;
+    }
+
+    if (numberOfPeople === "" || numberOfPeople <= 1) numberOfPeople = 1;
+
+    const tipPerPerson = (billAmount * tipAmount) / numberOfPeople;
+    document.getElementById('tipPerPerson').innerHTML = "$ " + tipPerPerson.toString();
+    document.getElementById('billPerPerson').innerHTML = "$ " + ((billAmount / numberOfPeople) + tipPerPerson).toString();
 }
